72b0c52f99bbedb71c7a3e898c82f0b4864d1142
LayK <layton.knight96@gmail.com>
2022-01-26 19:15:59 -0600

    Final Push for Sprite-Animation Projects

--- Sprite-Animation/sketch.js
+++ Sprite-Animation/sketch.js
@@ -1,61 +1,89 @@
 var spelunk;
+var eskimo;
+var bg;
 
 function setup(){
     createCanvas(800,400);
 
-    //create a sprite and add 3 animations
-    spelunk = createSprite(80,80,80,80);
+    //For Bonus/fun I am gonna add background for my sprites to walk on
+    bg = loadImage('assets/combo.png');
+
+    //create 2 sprites and add 3 animations
+    spelunk = createSprite(80,125,80,80);
+    eskimo = createSprite(80,320,80,80);
 
     //label each frame, then add to an animation for walking
     var myAnimation = spelunk.addAnimation('standing','assets/man_stand01.png');
+    var myAnimation2 = eskimo.addAnimation('eskimo_standing','assets/eskimo_stand01.png');
 
     //offC and offY is the distance of animation from the center of sprite
     //I have to play around to make sure they stay the centered, even with different heights
-    myAnimation.offY=18;
+    myAnimation.offY=4;
+    myAnimation2.offY=4;
 
     spelunk.addAnimation('walking','assets/man_walk01.png','assets/man_walk02.png','assets/man_walk03.png','assets/man_walk04.png','assets/man_walk05.png','assets/man_walk06.png','assets/man_walk07.png','assets/man_walk08.png');
+    //spelunk.addAnimation('crawling_down','assets/man_crawl01.png','assets/man_crawl02.png');
+    spelunk.addAnimation('crawling','assets/man_crawl02.png','assets/man_crawl03.png','assets/man_crawl04.png');
+    //spelunk.addAnimation('crawling_up','assets/man_crawl02.png','assets/man_crawl01.png');
+    
+    eskimo.addAnimation('eskimo_walking','assets/eskimo_walk01.png','assets/eskimo_walk02.png','assets/eskimo_walk03.png','assets/eskimo_walk04.png','assets/eskimo_walk05.png','assets/eskimo_walk06.png','assets/eskimo_walk07.png','assets/eskimo_walk08.png');
+    //spelunk.addAnimation('crawling_down','assets/man_crawl01.png','assets/man_crawl02.png');
+    eskimo.addAnimation('eskimo_crawling','assets/eskimo_crawl02.png','assets/eskimo_crawl03.png','assets/eskimo_crawl04.png');
+    //spelunk.addAnimation('crawling_up','assets/man_crawl02.png','assets/man_crawl01.png');
 
 
 }
 
 function draw(){
-    background(255,255,255);
+    background(bg);
 
-    //if mouse is to the left
-    if(mouseX < spelunk.position.x - 10) {
-        spelunk.changeAnimation('moving');
-        //flip horizontally
-        spelunk.mirrorX(-1);
-        //negative x velocity: move left
-        spelunk.velocity.x = -2;
+    //If "a" key is pressed, move left
+    if(keyIsDown(65)) {
+        //If Pressing "Left-Shift" key, then crawl while moving same direction
+        if(keyIsDown(16)){
+          spelunk.changeAnimation('crawling');
+          eskimo.changeAnimation('eskimo_crawling');
+        }
+        //Not Pressing Shift key, just do normal walk
+        else{
+          spelunk.changeAnimation('walking');
+          eskimo.changeAnimation('eskimo_walking');
+          //flip horizontally
+          spelunk.mirrorX(-1);
+          eskimo.mirrorX(-1);
+          //negative x velocity: move left
+          spelunk.velocity.x = -2;
+          eskimo.velocity.x = -2;
+        }
       }
-      else if(mouseX > spelunk.position.x + 10) {
-        spelunk.changeAnimation('moving');
-        //unflip
-        spelunk.mirrorX(1);
-        spelunk.velocity.x = 2;
+      //If "d" key is pressed, move right
+     else if(keyIsDown(68)) {
+       //If Pressing "Left-Shift" key, then crawl while moving same direction
+       if(keyIsDown(16)){
+        spelunk.changeAnimation('crawling');
+        eskimo.changeAnimation('eskimo_crawling');
+       }
+       //Not Pressing Shift key, just do normal walk
+       else{
+          spelunk.changeAnimation('walking');
+          eskimo.changeAnimation('eskimo_walking');
+          //unflip
+          spelunk.mirrorX(1);
+          spelunk.velocity.x = 2;
+          eskimo.mirrorX(1);
+          eskimo.velocity.x = 2;
+       }
       }
-      else {
+    else {
         //if close to the mouse, don't move
-        spelunk.changeAnimation('floating');
+        spelunk.changeAnimation('standing');
         spelunk.velocity.x = 0;
+        eskimo.changeAnimation('eskimo_standing');
+        eskimo.velocity.x = 0;
+
       }
     
-      if(mouseIsPressed) {
-        //the rotation is not part of the spinning animation
-        spelunk.rotation -= 10;
-        spelunk.changeAnimation('spinning');
-      }
-      else
-        spelunk.rotation = 0;
-    
-      //up and down keys to change the scale
-      //note that scaling the image quality deteriorates
-      //and scaling to a negative value flips the image
-      if(keyIsDown(UP_ARROW))
-      spelunk.scale += 0.05;
-      if(keyIsDown(DOWN_ARROW))
-      spelunk.scale -= 0.05;
+
     
       //draw the sprite
       drawSprites();
