7aa26820543ae2c4b9f0879773035df51a9da48a
LayK <layton.knight96@gmail.com>
2022-02-21 20:56:00 -0600

    Sampler-Push

--- Sampler/sketch.js
+++ Sampler/sketch.js
@@ -1,11 +1,55 @@
-var wave;
+let button;
+let slider;
+let count = 1;
+let effect;
+let sounds = {
+  android: "assets/Android.mp3",
+  stopTheCap: "assets/Stop-The-Cap.mp3",
+  boom: "assets/Vine-Boom.mp3",
+  dontClick: "assets/got-eem.mp3",
+  yeet: "assets/yeet.mp3",
+  HomeResonance: "assets/Home-Resonance.mp3",
+  Break: "assets/Want-A-Break-From-The-ADs.mp3"
+};
 
-function setup(){
-    createCanvas(100,100);
+function setup() {
+  createCanvas(500, 500);
+  background("lightgreen");
 
-    wave = new p5.Oscillator();
+  chorus = new Tone.Chorus();
+  pitchShift = new Tone.PitchShift();
+  const multiplayer = new Tone.Players(sounds);
+  multiplayer.volume.value = -20;
+  multiplayer.chain(chorus, pitchShift, Tone.Master);
+
+  for (const [key, value] of Object.entries(sounds)) {
+    button = createButton(key);
+    button.position(20, 20 * count);
+    button.mousePressed(() => {
+      multiplayer.player(key).start();
+    });
+    count++;
+  }
+  textSize(10);
+  text("click an audio sample to play sounds \nclick, drag, and hold the slider to add effect", 110, 30);
+  
+  text("CHORUS - \nadds stadium effect \nto audio", 35, 400);
+  chorusSlider = createSlider(0, 100, 0);
+  chorusSlider.position(20, 300);
+  chorusSlider.style("transform", "rotate(-90deg)");
+
+
+  text(
+    "PITCH SHIFT - \nUp on slider to slow \nDown on Slider for normal speed",
+    160,
+    400
+  );
+  pitchShiftSlider = createSlider(0, 100, 0);
+  pitchShiftSlider.position(110, 300);
+  pitchShiftSlider.style("transform", "rotate(-90deg)");
 }
 
-function draw(){
-    background(51);
+function draw() {
+  chorus.wet.value = chorusSlider.value() / 100;
+  pitchShift.wet.value = pitchShiftSlider.value() / 100;
 }
